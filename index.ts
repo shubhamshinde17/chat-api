2cd2cfa3f319c3630a39578a1a33f2e9447d6024
shubhamshinde17 <shubhamshinde266@gmail.com>
2022-07-30 23:51:53 +0530

    ..

--- index.ts
+++ index.ts
@@ -30,6 +30,7 @@ io.on('connection', (socket) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('listening on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${PORT}`);
 });
